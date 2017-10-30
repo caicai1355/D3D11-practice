c9c80bf14ad5f8c56c488168385c922250dfe75c
caicai <472272029@qq.com>
2017-10-31 00:14:47 +0800

    2017-10-30 03：改回来改回来

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //#pragma comment(lib, "d3d11.lib")
 //#pragma comment(lib, "d3dx11.lib")
-//#pragma comment(lib, "d3dx10.lib")11
+//#pragma comment(lib, "d3dx10.lib")
 
 #include <Windows.h>
 #include <D3D11.h>
