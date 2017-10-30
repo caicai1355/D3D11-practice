6ecf12ad019fc7c036d1282141f13c32fe258132
caicai <472272029@qq.com>
2017-10-31 00:07:09 +0800

    2017-10-30 02：尝试测试一下

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //#pragma comment(lib, "d3d11.lib")
 //#pragma comment(lib, "d3dx11.lib")
-//#pragma comment(lib, "d3dx10.lib")
+//#pragma comment(lib, "d3dx10.lib")11
 
 #include <Windows.h>
 #include <D3D11.h>
